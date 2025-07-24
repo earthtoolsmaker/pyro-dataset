0a9426a9a3c6917fc7d7a2299dabf47cab9e885d
Chouffe <arthur@caillau.me>
2025-07-24 19:23:33 +0200

    fix: tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -4,13 +4,7 @@ Tests for the parser utility functions.
 
 import pytest
 
-from pyro_dataset.parsers import parse_pyronear_camera_details
-
-s1 = "pyronear_valbonne_3_2023_11_02T07_05_56.jpg"
-s2 = "pyronear_serre-de-barre-310_2024-09-02T13-12-23.jpg"
-s3 = "pyronear_serre-de-barre-250_2024-08-25T11-19-22.jpg"
-s4 = "Pyronear_test_DS_00000328.jpg"
-s5 = "pyronear_st_peray_2_2023_07_12T13_24_14.jpg"
+from pyro_dataset.filepaths.parsers import parse_pyronear_camera_details
 
 
 @pytest.mark.parametrize(
